b8307f54f2d616c8f1628a4bdf29970fef52ca40
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2023-08-23 21:17:41 +0530

    admin ..

--- lib/screens/privacy_policy.dart
+++ lib/screens/privacy_policy.dart
@@ -1,21 +1,28 @@
+import 'package:business_bridge/models/policy.dart';
+import 'package:business_bridge/provider/data_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class privacy_policy extends StatefulWidget{
+class privacy_policy extends ConsumerStatefulWidget{
+
   @override
-  State<privacy_policy> createState() => _privacy_policyState();
+  ConsumerState<privacy_policy> createState() => _privacy_policyState();
 }
 
-class _privacy_policyState extends State<privacy_policy> {
+class _privacy_policyState extends ConsumerState<privacy_policy> {
   @override
   Widget build(BuildContext context) {
+    final policies = ref.watch(policyProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Privacy policy"),
       ),
       body: SingleChildScrollView(
         child: Column(
-
+          children: [
+          ],
         ),
       ),
     );
